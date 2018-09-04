09228dd4a7a314d426e48dec23bc97a344871e46
Shannon <sadair9@yahoo.com>
2018-09-04 09:34:55 -0600

    login designed

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,38 @@
 <template>
-    <h1>Login</h1>
+<div id='signup'>
+  <v-form ref="form" v-model="valid" lazy-validation >
+     <v-text-field
+      v-model="username"
+      :rules='usernameRules'
+      label="Username"
+      required
+    ></v-text-field>
+     <v-text-field
+      v-model="password"
+      :rules='passwordRules'
+      label="Password"
+      type="password"
+      required
+    ></v-text-field>
+    <v-btn
+      :disabled="!valid"
+      @click="submit"
+      color=#47B784
+    >
+      LOG IN
+    </v-btn>
+
+  </v-form>
+  <p>Don't have an account? <router-link to="/signup">Sign up</router-link> </p>
+</div>
 </template>
+
+<script>
+  export default {
+    name: 'hello-world',
+    data: () => ({
+      username: '',
+      password: '',
+    }),
+  }
+</script>
