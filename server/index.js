2f993088b98c2a39f7a3fc48fba1e48f49ff7d76
Shannon <sadair9@yahoo.com>
2018-09-04 12:59:38 -0600

    formatting

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser')
 
 const app = express();
 app.use(bodyParser.json())
-
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
