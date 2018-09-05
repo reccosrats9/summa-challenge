f36adb1e494a513059baa82ec403a9b41090d12e
Shannon <sadair9@yahoo.com>
2018-09-05 08:27:35 -0600

    set up controller, user login, basic vuex

--- server/index.js
+++ server/index.js
@@ -1,38 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const cont= require('./controller')
 
-const app = express();
+const app = express()
 app.use(bodyParser.json())
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-const users = [
-    {
-        name: 'Sara Hagstrom',
-        username: 'shagstrom',
-        email: 'sara@gmail.com',
-        password: 'passwordpassword'
-    },
-    {
-        name: 'Joe Blank',
-        username: 'jblank',
-        email: 'joe@gmail.com',
-        password: 'passwordpassword'
-    },
-    {
-        name: 'Ali Skalla',
-        username: 'askalla',
-        email: 'ali@gmail.com',
-        password: 'passwordpassword'
-    },
-]
-
-app.get('/users', (req, res) => {
-    res.status(200).send(users)
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
 })
+
+app.post('/login', cont.login)
+app.get('/users', cont.getUsers)
 
 const port = 3838;
 
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,16 +1,27 @@
 <template>
 <div>
     <h1>Users</h1>
+    <v-card v-for="user in users" :key="user.email" class='userCard'>
+        <h3 >{{user.name}}</h3>
+        <h5 >{{user.username}}</h5>
+        <h5 >{{user.email}}</h5>
+    </v-card>
 </div>
 </template>
 
 <script>
 import axios from 'axios'
+import { mapState } from 'vuex'
+
 export default {
     name: 'users',
     data: ()=>({
         users: []
     }),
+    computed: mapState([
+        'user',
+        'loggedIn'
+    ]),
     mounted(){
         axios.get('http://localhost:3838/users').then(res=>{
             this.users=res.data
