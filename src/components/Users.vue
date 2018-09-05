f422d5fd59fe3bdaec607d2aa52dbb634a150715
Shannon <sadair9@yahoo.com>
2018-09-05 09:16:52 -0600

    differentiate users

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,6 +1,11 @@
 <template>
 <div>
     <h1>Users</h1>
+     <v-card  class='userCard' color='#47B784'>
+        <h3 >{{user.name}}</h3>
+        <h5 >{{user.username}}</h5>
+        <h5 >{{user.email}}</h5>
+    </v-card>
     <v-card v-for="user in users" :key="user.email" class='userCard'>
         <h3 >{{user.name}}</h3>
         <h5 >{{user.username}}</h5>
@@ -24,7 +29,11 @@ export default {
     ]),
     mounted(){
         axios.get('http://localhost:3838/users').then(res=>{
-            this.users=res.data
+            let users=res.data
+            console.log(this.user)
+            let updated=users.filter(user=>user.name!==this.user.name)
+            console.log(updated)
+            this.users=updated
         }
         )
     } 
@@ -32,6 +41,10 @@ export default {
 </script>
 
 <style>
+.currentUser{
+    background: #47B784;
+    color: white;
+}
 .userCard{
     width: 200px;
     margin: auto;
